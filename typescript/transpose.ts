a758ccd0331e4ca4cc8e7f5567ab399542b6105a
ryinner <vinograd18062002@gmail.com>
2024-09-12 16:24:33 +0500

    transpose

---
+++ typescript/transpose.ts
@@ -0,0 +1,26 @@
+export function transpose(strings: string[]): string[] {
+  if (strings.length === 0) {
+    return strings;
+  }
+  return strings.reduce<string[]>((transposed, string, stringIndex) => {
+    const [stringWithMaxCharacter] = strings
+      .slice(stringIndex)
+      .toSorted((a, b) => b.length - a.length);
+    const maxCharacterStringIndex = strings.indexOf(stringWithMaxCharacter);
+    const { length: maxCharactersInString } = stringWithMaxCharacter;
+    if (
+      string.length < maxCharactersInString &&
+      stringIndex < maxCharacterStringIndex
+    ) {
+      string = string + " ".repeat(maxCharactersInString - string.length);
+    }
+    [...string].forEach((character, characterIndex) => {
+      if (transposed[characterIndex] !== undefined) {
+        transposed[characterIndex] += character;
+      } else {
+        transposed[characterIndex] = character;
+      }
+    });
+    return transposed;
+  }, []);
+}
